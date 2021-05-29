3ea24746c96e29fe76d1cc63cd40ca17a4f2fd2a
Vlad <vladvdobrynin@gmail.com>
2021-05-29 21:36:38 +0300

    load ticker data as json

--- server/src/ticker_warehouse/ticker_warehouse.py
+++ server/src/ticker_warehouse/ticker_warehouse.py
@@ -1,11 +1,12 @@
 import yfinance as yf
-from typing import List
+import pandas as pd
+from typing import Dict, List
 
 class TickerWarehouse:
 
-    _asset_info = {}
-    _asset_history = {}
-    _asset_set = set()
+    _ticker_info = {}
+    _ticker_history:Dict[str, pd.DataFrame] = {}
+    _ticker_set = set()
 
 
     _preimport = ["AAPL", "MSFT", "AMZN", "KO", "NKE"]
@@ -13,16 +14,23 @@ class TickerWarehouse:
     def __init__(self):
         self._import_start(self._preimport)
 
-    def import_asset(self, ticker_string:str):
-        if ticker_string not in self._asset_set:
+    def import_ticker(self, ticker_string:str):
+        if ticker_string not in self._ticker_set:
             ticker = yf.Ticker(ticker_string)
-            self._asset_info[ticker_string] = ticker
-            self._asset_history[ticker_string] = ticker.history(period="max", interval="1d")
+            self._ticker_info[ticker_string] = ticker
+            self._ticker_history[ticker_string] = ticker.history(period="max", interval="1d")
+            self._ticker_set.add(ticker_string)
 
 
     def _import_start(self, array_preimport: List):
         for ticker_string in array_preimport:
-            self.import_asset(ticker_string)
+            self.import_ticker(ticker_string)
 
     def get_all_history(self):
-        return self._asset_history
+        return self._ticker_history
+
+    def get_all_ticker_history(self, ticker_name: str):
+        return self._ticker_history[ticker_name]
+
+    def get_all_ticker_history_as_json(self, ticker_name:str):
+        return self.get_all_ticker_history(ticker_name).to_json(orient="index")
