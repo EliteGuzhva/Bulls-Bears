776fcd3436deaf66e26c3633aeb2064ce7d592dc
EliteGuzhva <nik_finger@mail.ru>
2021-06-22 18:20:02 +0300

    [fix] Added custom port for flask app

--- server/src/main.py
+++ server/src/main.py
@@ -1,6 +1,10 @@
+import os
 from .flask import create_app
 
 
 if __name__ == '__main__':
+    port = os.getenv("PORT", 5000)
+
     app = create_app()
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=port)
+
